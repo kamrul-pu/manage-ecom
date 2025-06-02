403582cad3b8e634036de7f03d522ef987337846
Kamrul Hasan <kamrul@Kamruls-MacBook-Air.local>
2025-06-02 22:50:46 +0600

    filter by sku created

--- app/inventory/views/inventory_request.py
+++ app/inventory/views/inventory_request.py
@@ -10,9 +10,15 @@ from inventory.serializers.inventory_request import (
 
 # InventoryRequestStock Views
 class InventoryRequestList(generics.ListCreateAPIView):
-    queryset = InventoryRequest().get_all_actives()
     serializer_class = InventoryRequestListSerializer
     permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        queryset = InventoryRequest().get_all_actives()
+        sku = self.request.query_params.get("sku", None)
+        if sku:
+            queryset = queryset.filter(sku=sku)
+        return queryset
 
 
 class InventoryRequestDetail(generics.RetrieveUpdateDestroyAPIView):
